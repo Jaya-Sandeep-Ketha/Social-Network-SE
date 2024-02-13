45ae9dfba2bf198788d18d76fae524bb337764c7
jketha <kethajayasandeep1254@gmail.com>
2024-02-12 20:15:20 -0500

    Login Page FrontEnd Created

---
+++ Semesters/Spring_2024/SE/SE_Project/connectsport/src/App.js
@@ -0,0 +1,44 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginForm from './Components/LoginForm';
+import RegisterForm from './Components/RegisterForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
